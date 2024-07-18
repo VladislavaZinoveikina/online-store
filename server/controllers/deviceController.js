07e3ea26bfe3b5afc19530d9541a617473142319
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2024-07-18 18:48:54 +0200

    added db models, routes

---
+++ server/controllers/deviceController.js
@@ -0,0 +1,15 @@
+class DeviceController {
+    async create(req, res) {
+
+    }
+
+    async login(req, res) {
+        
+    }
+
+    async check(req, res) {
+        
+    }
+}
+
+module.exports = new DeviceController();
