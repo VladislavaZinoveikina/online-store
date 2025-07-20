859607aa786b207f49aad8a7df939523ef37a96e
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-07-20 03:15:00 +0200

    pagination done

--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -6,15 +6,26 @@ import DeviceList from "../components/DeviceList";
 import { observer } from "mobx-react-lite";
 import { Context } from "../index";
 import { fetchBrands, fetchDevices, fetchTypes } from "../http/deviceAPI";
+import Pages from "../components/Pages";
 
 const Shop = observer(() => {
-    const {device} = useContext(Context);
+    const { device } = useContext(Context);
 
     useEffect(() => {
         fetchTypes().then(data => device.setTypes(data))
         fetchBrands().then(data => device.setBrands(data))
-        fetchDevices().then(data => device.setDevices(data.rows))
-    }, [])  
+        fetchDevices(null, null, 1, 2).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalDeviceCount(data.count)
+        })
+    }, [])
+
+    useEffect(() => {
+        fetchDevices(device.selectedType.id, device.selectedBrand.id, device.page, 2).then(data => {
+            device.setDevices(data.rows)
+            device.setTotalDeviceCount(data.count)
+        })
+    }, [device.page, device.selectedType, device.selectedBrand])
 
     return (
         <Container>
@@ -25,6 +36,7 @@ const Shop = observer(() => {
                 <Col md={9}>
                     <BrandBar />
                     <DeviceList />
+                    <Pages />
                 </Col>
             </Row>
         </Container>
