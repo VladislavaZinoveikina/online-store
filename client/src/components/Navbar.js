30628873893cc9db6fd135dbc93aed5d3c34d758
Владислава Зиновейкина <107256699+VladislavaZinoveikina@users.noreply.github.com>
2025-07-21 10:08:58 +0200

    shopping cart created

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,15 +8,18 @@ import { NavLink } from "react-router-dom";
 import { ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE } from "../utils/consts";
 import { observer } from "mobx-react-lite";
 import { useNavigate } from "react-router-dom";
+import "../index.css";
 
 const NavBar = observer(() => {
     const { user } = useContext(Context);
     const navigate = useNavigate();
+    let [openCart, setOpenCart] = useState(false);
 
     const logOut = () => {
         user.setUser({})
         user.setIsAuth(false)
     }
+
 
     return (
         <Navbar bg="dark" data-bs-theme="dark">
@@ -26,8 +29,20 @@ const NavBar = observer(() => {
                     <Nav className="ms-auto">
                         <Button
                             variant="outline-light"
+                            onClick={() => setOpenCart(openCart = !openCart)}
+                            className={`shopping-cart me-4 ${openCart && 'active'}`}
+                        >
+                            <img src="/white-shopping-cart.png" alt="shopping-cart" width={20} height={20} />
+                            {openCart && (
+                                <div className="shop-cart"></div>
+                            )}
+                        </Button>
+                        <Button
+                            variant="outline-light"
                             onClick={() => navigate(ADMIN_ROUTE)}
-                        >Admin dashboard</Button>
+                        >
+                            Admin dashboard
+                        </Button>
                         <Button
                             variant="outline-light"
                             onClick={() => logOut()}
